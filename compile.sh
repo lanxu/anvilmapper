1e7760ed0b88d5d0bd73f88beea7daa6fd112259
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2015-07-11 21:27:31 +0300

    Improve the compile.sh script to do everything automatically

--- compile.sh
+++ compile.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-javac\
- -sourcepath src:mapwriter_src\
- -d bin\
- src/anvilmapper/AnvilMapper.java
 
-# -sourcepath src:../mapwriter.git/src/main/java\
-# -classpath ../apache-log4j-2.0-rc1-bin/log4j-api-2.0-rc1.jar\
-# -sourcepath src\
-# -sourcepath ../apache-log4j-1.2.17/src/main/java\
-# -sourcepath ../apache-log4j-2.0-rc1-src/log4j-api/src/main/java\
+if [ ! -d "mapwriter" ] || [ ! -d "mapwriter/region/" ]
+then
+    git clone https://github.com/maruohon/mapwriter.git mapwriter
+    cd mapwriter
+    git checkout -b for_anvilmapper facb64e39fd070de8de4bcaed17a9945f7cb479b
+    cd ..
+fi
+
+if [ ! -d "bin" ]
+then
+    mkdir -p bin
+fi
+
+javac -sourcepath src:. -d bin src/anvilmapper/AnvilMapper.java
